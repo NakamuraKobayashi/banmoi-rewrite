5b3dc36e4434ba6300a6e51b04db8d05629029c8
Kobayashi <kobayashikenji126@gmail.com>
2022-07-10 14:21:32 -0700

    Update Slash

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const { Intents } = require("discord.js");
+const { Client, Intents } = require('discord.js');
+require('dotenv').config()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
