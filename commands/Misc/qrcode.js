0cac0a8da6fcb36be143160d5b8a9ff1fd29ebd9
tobycm <newtobycm@gmail.com>
2022-09-09 18:10:16 -0700

    qr let's go lmeo

--- commands/Misc/qrcode.js
+++ commands/Misc/qrcode.js
@@ -1,23 +1,26 @@
 const Color = "RANDOM", Discord = require("discord.js");
 
-module.exports = {
-  name: "qr",
-  aliases: ["qrcode", "qr-code"],
-  category: "misc",
-  userPerms: ["SEND_MESSAGES"],
-    clientPerms:['EMBED_LINKS'],
-  args: true,
-  premium: true,
-  description: "Gửi lại ảnh QR cho text hoặc ảnh bạn gửi!",
-  usage: "qr <nội_dung_tin_nhắn>",
-  run: async (client, message, args) => {
+function qr(client, message, args) {
     const Msg = args.join("+");
     const Embed = new Discord.MessageEmbed()
-    .setColor(Color)
-    .setImage(encodeURI(`https://chart.googleapis.com/chart?chl=${Msg}&chs=200x200&cht=qr&chld=H%7C0`))
-    .setTimestamp();
+        .setColor(Color)
+        .setImage(encodeURI(`https://chart.googleapis.com/chart?chl=${Msg}&chs=200x200&cht=qr&chld=H%7C0`))
+        .setTimestamp();
+
     message.delete()
 
     return message.channel.send({embeds:[Embed]});
-  }
+}
+
+module.exports = {
+    name: "qr",
+    aliases: ["qrcode", "qr-code"],
+    category: "misc",
+    userPerms: ["SEND_MESSAGES"],
+    clientPerms:['EMBED_LINKS'],
+    args: true,
+    premium: true,
+    description: "Gửi lại ảnh QR cho text hoặc ảnh bạn gửi!",
+    usage: "qr <nội_dung_tin_nhắn>",
+    run: qr
 };
