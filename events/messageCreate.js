a94395fa6a1cbeed3cc63a1fb45577ef2f3ad3c8
tobycm <newtobycm@gmail.com>
2022-09-09 17:50:38 -0700

    pro python coder code javascript kết quả hết hồn

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,47 +1,41 @@
-const client = require('../index.js');
-// const db = require('../database');
-const {Collection} = require('discord.js')
-client.on('messageCreate', async message => {
-if(message.author.bot) return;
+async function messageCreate(message) {
+    if (message.author.bot) return;
+
+    let banmoi = message.client;
+    const prefix = 'moi!';
+
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd.length == 0) return;
+    let command = banmoi.commands.get(cmd);
+    if (!command) command = banmoi.commands.get(banmoi.aliases.get(cmd));
 
 
-  const prefix = 'moi!';
+    if (command.ownerOnly) {
+        if(!banmoi.config.OWNERID.includes(message.author.id)) {
+            return message.reply(`${message.member} You can't access owner commands`);
+        }
+    } 
 
+    if (!message.member.permissions.has(command.userPerms || [])) return message.channel.send(`Bạn không có quyền \`${command.userPerms || []}\` để dùng lệnh này`)
 
-  if(!message.content.startsWith(prefix)) return;
-  if(!message.member) message.member = await message.guild.fetchMember(message);
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if(cmd.length == 0) return;
-  let command = client.commands.get(cmd)
-  if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if (!message.guild.me.permissions.has(command.banmoiPerms || [])) return message.channel.send(`Bot không đủ quyền  \`${command.banmoiPerms || []}\` để thực thi lệnh`)
 
-      if(command) {
-        //OWNER ONLY
-        if(command.ownerOnly) {
-          if(!client.config.OWNERID.includes(message.author.id)) {
-            message.reply(`${message.member} You can't access owner commands`)
-            return;
-          }
-        } 
+    try {
+        command.run(banmoi, message, args, prefix)
     }
-    if(command) {
-
-      //USER PERMISSION
-      if(!message.member.permissions.has(command.userPerms || [])) return message.channel.send(`Bạn không có quyền \`${command.userPerms || []}\` để dùng lệnh này`)
-  
-      //BOT PERMISSION
-      if(!message.guild.me.permissions.has(command.clientPerms || [])) return message.channel.send(`Bot không đủ quyền  \`${command.clientPerms || []}\` để thực thi lệnh`)
-  
+    catch (error) {
+        console.log(error)
+        // lmeo lỗi code kìa haha
+        // no need anticrash.js anymore
     }
-
-  try {
-    command.run(client, message, args, prefix)
-  }
-  catch (error) {
-    console.log(error)
-    // lmeo lỗi code kìa haha
-    // no need anticrash.js anymore
-  }
 }
-);
+
+module.exports = {
+    name: "messageCreate",
+    func: messageCreate
+}
--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-const { Client,Collection, Discord } = require('discord.js')
+const { Banmoi } = require("./models/client")
 
-const fs = require('fs')
-const client = new Client({
-    partials: ["MESSAGE", "CHANNEL", "REACTION"],
-    shards: 'auto',
-      intents: 32767, allowedMentions: { parse: ['users', 'roles'], repliedUser: true }
-  });
-  const dotenv = require('dotenv')
-const envFile = dotenv.config()
+const { add_commands, add_events } = require("./handlers/command")
 
+const banmoi = new Banmoi(
+	{
+		partials: ["MESSAGE", "CHANNEL", "REACTION"],
+		shards: 'auto',
+		intents: 32767, allowedMentions: { parse: ['users', 'roles'], repliedUser: true }
+	}
+);
+
+add_commands(banmoi)
+add_events(banmoi)
 
 const token = process.env['token']
-  
-  module.exports = client;
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
 
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = fs.readdirSync("./commands/");
-client.interactions = new Collection()
-client.slashCommands = new Collection();
-client.anticrash = new Collection();
-client.config = require('./config.json');
-client.events = new Collection();
-// require("./handlers")(client);
-['command','anticrash','slashCommand'].forEach(handler => require(`./handlers/${handler}`)(client))
-
-client.snipes = new Map() //create a new map
-client.on('messageDelete', function(message, channel) {
-  client.snipes.set(message.channel.id, { //get the channel of message
-    content: message.content, //snipe the message that was deleted
-    author: message.author, //get the message author the the deleted message
-    image: message.attachments.first() ? message.attachments.first().proxyURL : null //get the deleted image if there is one
-  })
-})
-client.login(token);
+banmoi.login(token);
