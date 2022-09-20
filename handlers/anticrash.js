1381caddf4585e5d7b5a793b4d719953ea42a652
Kobayashi <kobayashikenji126@gmail.com>
2022-09-20 07:22:29 -0700

    add more commands

--- handlers/anticrash.js
+++ handlers/anticrash.js
@@ -1,4 +1,4 @@
- module.exports = client => {
+module.exports = client => {
     process.on('unhandledRejection', (reason, p) => {
          console.log(' [antiCrash] :: Unhandled Rejection/Catch');
          console.log(reason, p);
